247157be42456994e8cd622df5c27f9fb6442783
rahul0021-ts <supekar0021@gmail.com>
2026-04-01 14:48:04 +0530

    token time update 24h to 7d

--- routes/auth.js
+++ routes/auth.js
@@ -31,7 +31,7 @@ router.post(
       }
 
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN || "24h",
+        expiresIn: process.env.JWT_EXPIRES_IN || "7d",
       });
 
       return res.json({
@@ -80,7 +80,7 @@ router.post(
       });
 
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN || "24h",
+        expiresIn: process.env.JWT_EXPIRES_IN || "7d",
       });
 
       return res.status(201).json({
